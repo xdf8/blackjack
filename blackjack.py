208ce307d3d96c7e48ec159d68a7ba1a79104507
Eric Winter <ericwinter1996@gmail.com>
2022-10-09 00:26:18 +0200

    done? but looks bad

--- blackjack.py
+++ blackjack.py
@@ -1,23 +1,11 @@
+from time import sleep
+
+from functions import get_score
 from functions import get_user_action
 from functions import select_card_remove_from_deck
 
 card_colors = ["♥️", "♦️", "♠️", "♣️"]
 card_faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-card_values = {
-    "A": 11,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "J": 10,
-    "Q": 10,
-    "K": 10,
-}
 card_deck = []
 
 # generate all card combinations
@@ -25,6 +13,7 @@ for color in card_colors:
     for value in card_faces:
         card_deck.append(f"{value} of {color}")
 
+# game relevant stuff
 player_cards = [
     select_card_remove_from_deck(card_deck),
     select_card_remove_from_deck(card_deck),
@@ -34,10 +23,61 @@ dealer_cards = [
     select_card_remove_from_deck(card_deck),
 ]
 
-print(f"Dealer has: {dealer_cards}")
+dealer_score = get_score(dealer_cards)
+player_score = get_score(player_cards)
+
+if dealer_score == 21:
+    print(f"Dealer has: {dealer_cards}")
+    print(f"Your cards are: {player_cards}")
+    print("Dealer has blackjack!")
+    exit()
+
+print(f"Dealer has: {dealer_cards[0]} and [hidden]")
 print(f"Your cards are: {player_cards}")
+print(f"Your score is: {player_score}")
 
-user_action = get_user_action()
+if player_score == 21:
+    print("You have blackjack!")
+    exit()
 
-print(user_action)
+while user_action := get_user_action() == "h":
+    player_cards.append(select_card_remove_from_deck(card_deck))
+    player_score = get_score(player_cards)
+    print(f"Your cards are: {player_cards}")
+    print(f"Your score is: {player_score}")
+    if player_score > 21:
+        print("You went over 21, you lose!")
+        exit()
+    elif player_score == 21:
+        print("You have blackjack!")
+        exit()
+
 # dealer must hit on 16 and stand on 17 or more
+print("Dealers turn")
+print(f"Dealer has: {dealer_cards}")
+while dealer_score < 17:
+    sleep(1)
+    dealer_cards.append(select_card_remove_from_deck(card_deck))
+    dealer_score = get_score(dealer_cards)
+    print(f"Dealer has: {dealer_cards}")
+    print(f"Dealer score is: {dealer_score}")
+    sleep(1)
+    if dealer_score > 21:
+        print("Dealer went over 21, you win!")
+        exit()
+    elif dealer_score == 21:
+        print("Dealer has blackjack, you lose!")
+        exit()
+    elif dealer_score >= 17:
+        print("Dealer has 17 or more, dealer stands")
+        print(f"Dealer score is: {dealer_score}")
+
+sleep(1)
+
+if dealer_score > 17:
+    print(f"Dealer has a score of {dealer_score}, dealer stands")
+
+if player_score > dealer_score:
+    print("You win!")
+elif player_score < dealer_score:
+    print("Dealer wins!")
--- functions.py
+++ functions.py
@@ -8,7 +8,34 @@ def select_card_remove_from_deck(deck):
 
 
 def get_user_action():
-    user_action = input("Do you want to hit [h] or stand [h]?: ")
+    user_action = input("Do you want to hit [h] or stand [s]?: ")
     while user_action != "h" and user_action != "s":
         user_action = input("Please select a valid option [h, s]: ")
     return user_action
+
+
+def get_score(cards: list) -> int:
+    score = 0
+    card_values = {
+        "A": 11,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "10": 10,
+        "J": 10,
+        "Q": 10,
+        "K": 10,
+    }
+    for card in cards:
+        card_value = card.split()[0]
+        score += card_values[card_value]
+    # ace can count as 1 or 11
+    contains_ace = any([True if "A" in card else False for card in cards])
+    if score > 21 and contains_ace:
+        score -= 10
+    return score
